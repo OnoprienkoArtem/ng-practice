f064c107150a1debc4e1ac923c4f9796fa3524c6
OnoprienkoArtem <o.artem.e@gmail.com>
2019-05-24 17:54:51 +0300

    add child component

--- 4/movie-app/src/app/films-catalog/films-catalog.module.ts
+++ 4/movie-app/src/app/films-catalog/films-catalog.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FilmsListComponent } from './films-list/films-list.component';
 import { MainComponent } from './main/main.component';
+import { FilmItemComponent } from './films-list/film-item/film-item.component';
 
 @NgModule({
-  declarations: [FilmsListComponent, MainComponent],
+  declarations: [FilmsListComponent, MainComponent, FilmItemComponent],
   imports: [
     CommonModule
   ]
