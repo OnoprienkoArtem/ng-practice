87ef9da0bd6aca81de9b3c34a1acf243c866b675
OnoprienkoArtem <o.artem.e@gmail.com>
2019-06-04 21:58:19 +0300

    refactoring modules

--- 4/movie-app/src/app/app.module.ts
+++ 4/movie-app/src/app/app.module.ts
@@ -3,11 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
 import { FilmsCatalogModule } from './films-catalog/films-catalog.module';
 import { MaterialModule } from './material/material.module';
 
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
--- 4/movie-app/src/app/films-catalog/films-catalog.module.ts
+++ 4/movie-app/src/app/films-catalog/films-catalog.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { HttpClientModule } from '@angular/common/http';
 
+import { SharedModule } from '../shared/shared.module';
+import { MaterialModule } from '../material/material.module';
+
 import { FilmsListComponent } from './films-list/films-list.component';
 import { MainComponent } from './main/main.component';
 
-import { SharedModule } from '../shared/shared.module';
-import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [
