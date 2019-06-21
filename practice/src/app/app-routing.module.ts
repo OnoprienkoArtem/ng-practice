899f3d8d8f27be12a25167fd5e62fefd934da29b
OnoprienkoArtem <o.artem.e@gmail.com>
2019-06-21 22:28:01 +0300

    set new route for search-result component

--- practice/src/app/app-routing.module.ts
+++ practice/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './films-catalog/main/main.component';
 import { FilmsListComponent } from './films-catalog/films-list/films-list.component';
 import { ActorsListComponent } from './films-catalog/actors-list/actors-list.component';
+import { SearchResultComponent } from './films-catalog/search-result/search-result.component';
 
 const routes: Routes = [
   { path: "", pathMatch: "full", redirectTo: "main" },
   { path: "main", component: MainComponent },
   { path: "films-list", component: FilmsListComponent },
-  { path: "actors-list", component: ActorsListComponent }
+  { path: "actors-list", component: ActorsListComponent },
+  { path: "search-result", component: SearchResultComponent }
 ];
 
 @NgModule({
