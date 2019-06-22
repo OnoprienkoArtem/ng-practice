32fe4a4878d309602ee0b8ee9d11cf96e141ee1e
OnoprienkoArtem <o.artem.e@gmail.com>
2019-06-22 12:40:02 +0300

    add to main page actors part

--- practice/src/app/films-catalog/main/main.component.ts
+++ practice/src/app/films-catalog/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FilmService } from '../../services/film.service';
 import { Film } from '../../models/film';
+import { Actor } from '../../models/actor';
 
 @Component({
   selector: 'app-main',
@@ -13,6 +14,8 @@ export class MainComponent implements OnInit {
   public imgUrl: string = this.filmsService.midImgPath; 
   public films: Film[] = [];
   public filmsClone: any[] = [];
+  public actors: Actor[] = [];
+  private actorsClone: any[] = [];
 
   constructor(private filmsService: FilmService) { }
 
@@ -22,6 +25,17 @@ export class MainComponent implements OnInit {
         this.filmsClone = filmList.results;
         this.films = this.filmsClone.slice(0, 6);  
         if (this.filmsClone) {
+          this.spiner = false;
+        }
+      },
+      err => console.log("error", err)
+    ),
+    
+      this.filmsService.getPopularActors().subscribe(
+        (actorsList: any) => {
+          this.actorsClone = actorsList.results;
+          this.actors = this.actorsClone.slice(0, 6);
+          if (this.actorsClone) {
           this.spiner = false;
         }
       },
