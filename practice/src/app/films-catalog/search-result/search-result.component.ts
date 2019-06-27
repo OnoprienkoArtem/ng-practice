fde2c32c1917125d71bc4025b85a5805aa24cb11
OnoprienkoArtem <o.artem.e@gmail.com>
2019-06-27 22:37:24 +0300

    change film item component

--- practice/src/app/films-catalog/search-result/search-result.component.ts
+++ practice/src/app/films-catalog/search-result/search-result.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { SearchService } from '../../services/search.service';
+import { FilmService } from '../../services/film.service';
 import { LOCAL_CONFIG } from '../../config/config-api';
 import { ApiConfig } from '../../models/api';
 import { Router } from '@angular/router';
-
+import { Favorite } from '../../models/favorites';
+import { Bookmark } from '../../models/bookmark';
 
 @Component({
   selector: 'app-search-result',
@@ -23,7 +25,7 @@ export class SearchResultComponent implements OnInit {
     @Inject(LOCAL_CONFIG) public localConfig: ApiConfig,
     private router: Router,
     public searchServic: SearchService,
-
+    public filmsService: FilmService
   ) { }
 
   pageCount: number = 1; 
@@ -56,6 +58,8 @@ export class SearchResultComponent implements OnInit {
         this.searchItems = res.results;
         this.totalResalt = res.total_results;
         this.totalPages = res.total_pages;
+        this.getFavorite();
+        this.getBookmark();
         if (this.pageCount === this.totalPages) this.isDisabledActorsBtn = true;
         if (this.searchItems) {
           this.spiner = false;
@@ -69,6 +73,49 @@ export class SearchResultComponent implements OnInit {
     this.pageCount++; 
     if (this.pageCount === this.totalPages) this.isDisabledActorsBtn = true;    
     this.getSearchedList(this.searchFor, this.searchServic.search.searchValue, this.pageCount);
+  }
+
+
+  private getFavorite() {
+    this.filmsService.getFavorite(this.searchItems.map(item => item.id)).subscribe((favorites: Array<Favorite>) => {
+      const favoriteList = favorites.map(favorite => favorite._id);
+      this.searchItems.map(item => {
+        item.isFavorite = favoriteList.indexOf(item.id) > -1;
+      })
+    })
+  }
+
+  public addFilmToFavorit(id: number) {
+    const favoriteFilms = this.searchItems.find(item => {
+      return item.id === id;
+    });
+
+    if (favoriteFilms.isFavorite) {
+      this.filmsService.removeFromFavorite(id).subscribe(() => this.getFavorite());
+    } else {
+      this.filmsService.addToFavorite(id).subscribe(() => this.getFavorite());
+    }
+  }
+
+  private getBookmark() {
+    this.filmsService.getBookmark(this.searchItems.map(item => item.id)).subscribe((bookmarks: Array<Bookmark>) => {
+      const bookedList = bookmarks.map(bookmark => bookmark._id);
+      this.searchItems.map(item => {
+        item.isBooked = bookedList.indexOf(item.id) > -1;
+      })
+    })
+  }
+
+  public addFilmToBookmark(id: number) {
+    const bookmarkFilms = this.searchItems.find(item => {
+      return item.id === id;
+    });
+
+    if (bookmarkFilms.isBooked) {
+      this.filmsService.removeFromBookmark(id).subscribe(() => this.getBookmark());
+    } else {
+      this.filmsService.addToBookmark(id).subscribe(() => this.getBookmark());
+    }
   }
 
 
