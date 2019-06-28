d6f0aa54d41dd5e2cdd442ee3d9b7485075e9402
OnoprienkoArtem <o.artem.e@gmail.com>
2019-06-28 22:40:12 +0300

    add auth guard service

---
+++ practice/src/app/services/auth-guard.service.ts.service.ts
@@ -0,0 +1,38 @@
+import { CanActivate, CanActivateChild, CanLoad, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
+
+
+  constructor(
+    private authService: AuthService,
+    private router: Router) {
+  }
+
+  canActivate() {
+
+    const isLogin = this.authService.isLoggedIn();
+    if (isLogin) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+
+  canLoad() {
+    return this.canActivate();
+  }
+
+  canActivateChild() {
+    return this.canActivate();
+  }
+
+}
+
