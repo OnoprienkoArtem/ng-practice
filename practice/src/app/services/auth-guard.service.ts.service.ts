98475cc131fe12ab8b5d1cdc495414b9cb5e1028
OnoprienkoArtem <o.artem.e@gmail.com>
2019-06-29 15:51:26 +0300

    add auth service

--- practice/src/app/services/auth-guard.service.ts.service.ts
+++ practice/src/app/services/auth-guard.service.ts.service.ts
@@ -1,5 +1,5 @@
 import { CanActivate, CanActivateChild, CanLoad, Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
+import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
 
 @Injectable({
