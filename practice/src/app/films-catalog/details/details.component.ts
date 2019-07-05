91d6024bb1fc3a5251e903d71812e535d1947265
OnoprienkoArtem <o.artem.e@gmail.com>
2019-07-05 23:53:12 +0300

    work with tamplate details page

--- practice/src/app/films-catalog/details/details.component.ts
+++ practice/src/app/films-catalog/details/details.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
+  public spiner: boolean = true;
+
   constructor() { }
 
   ngOnInit() {
+    this.spiner = false;
+ 
   }
 
 }
