97ac7bc81cc3816ef19c85caaa1217adaadf6d62
OnoprienkoArtem <o.artem.e@gmail.com>
2019-07-06 15:43:55 +0300

    work with details page

--- practice/src/app/films-catalog/details/details.component.ts
+++ practice/src/app/films-catalog/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -8,11 +9,23 @@ import { Component, OnInit } from '@angular/core';
 export class DetailsComponent implements OnInit {
 
   public spiner: boolean = true;
+  public currentRoute: string;
+  public filmDetails: boolean = true;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     this.spiner = false;
+
+    this.router.events.subscribe(
+      (event: any) => {
+        if (event instanceof NavigationEnd) {
+          this.currentRoute = event.url;          
+          if (this.currentRoute === '/films/details') {
+            this.filmDetails = false;
+          }
+        }
+      });
  
   }
 
