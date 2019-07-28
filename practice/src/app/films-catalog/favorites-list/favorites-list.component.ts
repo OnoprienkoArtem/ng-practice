55e4c746512367745151e258e96b35f7fea9840f
OnoprienkoArtem <o.artem.e@gmail.com>
2019-07-28 16:21:09 +0300

    work with favorite page

--- practice/src/app/films-catalog/favorites-list/favorites-list.component.ts
+++ practice/src/app/films-catalog/favorites-list/favorites-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FilmService } from '../../services/film.service';
+import { Film } from '../../models/film';
 
 @Component({
   selector: 'app-favorites-list',
@@ -7,9 +9,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritesListComponent implements OnInit {
 
-  constructor() { }
+  private totalPages: number;
+  private page: number = 1;
+  public spiner: boolean = true;
+  public isDisabledFilmsBtn: boolean = false;
+  public films: Film[] = [];
+
+  constructor(public filmsService: FilmService) { }
 
   ngOnInit() {
+    this.getOnePagePopularFilms(this.page);
+  }
+
+  getOnePagePopularFilms(page) {
+
+    const userId = localStorage.getItem('user_id');
+    const sessionId = localStorage.getItem('session_id');
+    this.filmsService.getListOfFavotitesFilms(userId, sessionId).subscribe((favorites: any) => {                    
+        // this.filmsService.changefavoriteNumber(favorites.total_results);
+        console.log(favorites);
+        this.totalPages = favorites.total_pages;       
+        this.films = favorites.results;  
+        if (this.films) {
+          this.spiner = false;
+        }
+      }
+    )
+            
+    // this.filmsService.getPopularFilms(page).subscribe(
+    //   (filmList: any) => { 
+    //     this.totalPages = filmList.total_pages;       
+    //     this.films = filmList.results;
+
+    //     // this.getFavorite();
+       
+
+    //     if (this.films) {
+    //       this.spiner = false;
+    //     }
+    //   },
+    //   err => console.log("error", err)
+    // )
+    
+  }
+
+  public nextFilmsPage() {   
+    this.page++;
+    this.getOnePagePopularFilms(this.page);
+    this.isDisabledFilmsBtn = this.page === this.totalPages ? true : false;  
   }
 
 }
