d45d7539026a0d9f4e21b5d80e7d9b6bbf3dfe1d
OnoprienkoArtem <o.artem.e@gmail.com>
2019-08-29 21:32:29 +0300

    refactoring codes

--- practice/src/app/films-catalog/main/main.component.ts
+++ practice/src/app/films-catalog/main/main.component.ts
@@ -5,8 +5,6 @@ import { Film } from '../../models/film';
 import { Actor } from '../../models/actor';
 import { LOCAL_CONFIG } from '../../config/config-api';
 import { ApiConfig } from '../../models/api';
-
-
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -15,40 +13,41 @@ import { ApiConfig } from '../../models/api';
 export class MainComponent implements OnInit {
 
   public spiner: boolean = true;
-  public imgUrl: string = this.localConfig.midImgPath; 
+  public imgUrl: string = this.localConfig.midImgPath;
   public films: Film[] = [];
   public filmsClone: any[] = [];
   public actors: Actor[] = [];
   private actorsClone: any[] = [];
 
   constructor(
-    private filmsService: FilmService, 
+    private filmsService: FilmService,
     public actorService: ActorService,
     @Inject(LOCAL_CONFIG) public localConfig: ApiConfig
-    ) { }
+  ) { }
 
   ngOnInit() {
+
     this.filmsService.getPopularFilms().subscribe(
       (filmList: any) => {
         this.filmsClone = filmList.results;
-        this.films = this.filmsClone.slice(0, 6);  
+        this.films = this.filmsClone.slice(0, 6);
         if (this.filmsClone) {
           this.spiner = false;
         }
       },
       err => console.log("error", err)
     ),
-    
-    this.actorService.getPopularActors().subscribe(
-      (actorsList: any) => {
-        this.actorsClone = actorsList.results;
-        this.actors = this.actorsClone.slice(0, 6);
-        if (this.actorsClone) {
-          this.spiner = false;
-        }
-      },
-      err => console.log("error", err)
-    )
+
+      this.actorService.getPopularActors().subscribe(
+        (actorsList: any) => {
+          this.actorsClone = actorsList.results;
+          this.actors = this.actorsClone.slice(0, 6);
+          if (this.actorsClone) {
+            this.spiner = false;
+          }
+        },
+        err => console.log("error", err)
+      )
   }
 
 }
