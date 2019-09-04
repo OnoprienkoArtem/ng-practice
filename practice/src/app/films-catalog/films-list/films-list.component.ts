aa9d2e97f1dfd2647501daf682ee62010285015f
OnoprienkoArtem <o.artem.e@gmail.com>
2019-09-04 22:12:36 +0300

    carent page on filmslist component

--- practice/src/app/films-catalog/films-list/films-list.component.ts
+++ practice/src/app/films-catalog/films-list/films-list.component.ts
@@ -32,23 +32,17 @@ export class FilmsListComponent implements OnInit {
   }
 
   getOnePagePopularFilms(page) {
-
     this.filmsService.getPopularFilms(page).subscribe(
-      (filmList: any) => {
-        console.log(filmList);
-        this.totalPages = filmList.total_pages;
-
-
+      (filmList: any) => {   
         // for pagination
         this.total = filmList.total_pages;
-        this.pagePag = filmList.page;
-       
-
+        this.pagePag = filmList.page;       
+        // films list
+        this.totalPages = filmList.total_pages;
         this.page = filmList.page;
         this.totalResult = filmList.total_results;
         this.films = filmList.results;
         this.filmsService.getFavoriteFilms(this.films, this.userId, this.sessionId);
-
         if (this.films) {
           this.spiner = false;
         }
@@ -57,11 +51,7 @@ export class FilmsListComponent implements OnInit {
     );
   }
 
-  // public nextFilmsPage() {
-  //   this.page++;
-  //   this.getOnePagePopularFilms(this.page);
-  //   this.isDisabledFilmsBtn = this.page === this.totalPages ? true : false;
-  // }
+
 
   public addFilmToFavorit(id: number) {
     const favoriteFilms = this.films.find(item => item.id === id);
