648a6b6ce56c2025413f82dcab810a63c2074a81
OnoprienkoArtem <o.artem.e@gmail.com>
2019-09-08 23:29:28 +0300

    work with favorite component

--- practice/src/app/films-catalog/favorites-list/favorites-list.component.ts
+++ practice/src/app/films-catalog/favorites-list/favorites-list.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FilmService } from '../../services/film.service';
 import { Film } from '../../models/film';
-import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-
 @Component({
   selector: 'app-favorites-list',
   templateUrl: './favorites-list.component.html',
@@ -12,22 +10,21 @@ import { Observable } from 'rxjs';
 export class FavoritesListComponent implements OnInit {
   private userId = localStorage.getItem('user_id');
   private sessionId = localStorage.getItem('session_id');
-
   public totalPages: number;
   public totalResult$: Observable<number>; 
+  public totalResult: number;
   public page: number;
   public spinner: boolean = true; 
   public films: Film[] = [];
 
 
-  constructor(
-    public filmsService: FilmService,
-    private router: Router
-  ) { }
+  constructor(public filmsService: FilmService) { }
 
   ngOnInit() {   
-    this.getOnePagePopularFilms(this.filmsService.currentPageFavorites);
-    this.totalResult$ = this.filmsService.getfavoriteNumber();
+    this.getOnePagePopularFilms(this.filmsService.currentPageFavorites);    
+    this.totalResult$.subscribe(res => {
+      this.totalResult = res;     
+    })
   }
 
   public getOnePagePopularFilms(page) {
@@ -35,7 +32,6 @@ export class FavoritesListComponent implements OnInit {
       console.log(favorites);
 
       this.filmsService.getFavoriteFilmsList().subscribe(res => this.films = res);
-
       this.page = favorites.page;
       this.filmsService.changefavoriteNumber(favorites.total_results);
       this.totalPages = favorites.total_pages;
@@ -49,8 +45,7 @@ export class FavoritesListComponent implements OnInit {
   }
 
   public removeFilmFromFavorit(id: number) {
-    this.filmsService.markFavorite(id, false, this.films, this.userId, this.sessionId);
-  
+    this.filmsService.markFavorite(id, false, this.films, this.userId, this.sessionId);  
     this.filmsService.getFavoriteFilmsList().subscribe(res => {
         console.log(res);
       }
